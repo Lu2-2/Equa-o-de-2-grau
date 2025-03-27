68a29eda5afee91596278b92ed3ba2102e0125c7
Luis <luis.henriquenc10@gmail.com>
2025-03-27 12:54:35 -0300

    vai tomando

--- script.js
+++ script.js
@@ -5,18 +5,24 @@ function calcular(){
 
     // Aviso caso não tenha nada
     if(a, b, c == ""){
-        alert('Insira um numero')
+        alert('Não deixa as caixas vazias');
         return
     }
 
     // Delta
-    const delta = parseInt((b**2)+(-4*a*c))
-    alert("Δ: " + delta)
-
+    const delta = parseInt((b**2)+(-4*a*c));
+    if(delta<0){
+        alert('Delta negativo, sem raizes')
+        return
+    }
+    else{
+        alert("Δ: " + delta);
+    }
+    
     // X1 e X2
     const raizDeDelta = Math.sqrt(delta)
-    const x1 = parseFloat((-b+raizDeDelta)/(2*a))
-    const x2 = parseFloat((-b-raizDeDelta)/(2*a))
-    alert("x1: " + x1)
-    alert("x2: " + x2)
+    const x1 = parseFloat((-b+raizDeDelta)/(2*a));
+    const x2 = parseFloat((-b-raizDeDelta)/(2*a));
+    alert("x1: " + x1);
+    alert("x2: " + x2);
 }
