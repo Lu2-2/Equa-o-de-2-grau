923e28b6c4f7e4bf5d3957efd2b5c250bfa83600
Luis <luis.henriquenc10@gmail.com>
2025-03-27 09:13:54 -0300

    Funcional kkkkk

--- script.js
+++ script.js
@@ -3,13 +3,20 @@ function calcular(){
     var b = document.getElementById('b').value;
     var c = document.getElementById('c').value;
 
+    // Aviso caso não tenha nada
+    if(a, b, c == ""){
+        alert('Insira um numero')
+        return
+    }
+
     // Delta
     const delta = parseInt((b**2)+(-4*a*c))
-    alert(delta)
+    alert("Δ: " + delta)
 
     // X1 e X2
     const raizDeDelta = Math.sqrt(delta)
     const x1 = parseFloat((-b+raizDeDelta)/(2*a))
     const x2 = parseFloat((-b-raizDeDelta)/(2*a))
-    alert("x1: "+x1, "x2: "+x2)
+    alert("x1: " + x1)
+    alert("x2: " + x2)
 }
